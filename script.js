c0c4d68de8ae7fd4fa3a86e1ff53ac553f4ef097
Julia <jsgoransson@gmail.com>
2023-11-20 15:41:12 +0100

    Fixed buttons

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const baseUrl = "https://type.fit/api/quotes";
 const quoteWrapper = document.querySelector("#quote-wrapper");
 const quoteTextElement = document.querySelector("#quote-text");
+const allQuotesButton = document.querySelector("#all-quotes");
 const randomButton = document.querySelector("#random");
 const catgoriesButton = document.querySelector("#catagories");
 
@@ -8,6 +9,7 @@ randomButton.addEventListener("click", function () {
   const fetchRandomQuote = async () => {
     const response = await fetch(baseUrl);
     const quotes = await response.json();
+
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const randomQuote = quotes[randomIndex].text;
 
@@ -15,4 +17,20 @@ randomButton.addEventListener("click", function () {
   };
 
   fetchRandomQuote();
+});
+
+allQuotesButton.addEventListener("click", function () {
+  const fetchAllQuotes = async () => {
+    const response = await fetch(baseUrl);
+    const quotes = await response.json();
+
+    quoteWrapper.innerHTML = "";
+
+    quotes.forEach((quote) => {
+      const quoteElement = document.createElement("p");
+      quoteElement.textContent = quote.text;
+      quoteWrapper.appendChild(quoteElement);
+    });
+  };
+  fetchAllQuotes();
 });
