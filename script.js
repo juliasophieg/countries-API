714184e36f8838abfaa8bd1477a78c6eaa2c6574
Julia <jsgoransson@gmail.com>
2023-12-03 10:25:40 +0100

    Changed API to countries instead

--- script.js
+++ script.js
@@ -1,36 +1,106 @@
-const baseUrl = "https://type.fit/api/quotes";
-const quoteWrapper = document.querySelector("#quote-wrapper");
-const quoteTextElement = document.querySelector("#quote-text");
-const allQuotesButton = document.querySelector("#all-quotes");
+const baseUrl = "https://restcountries.com/v3.1/all";
+const countriesWrapper = document.querySelector("#countries-wrapper");
+const countryTextElement = document.querySelector("#country-text");
+const flagElement = document.querySelector("#flag");
+const continentElement = document.querySelector("#continent");
+const populationElement = document.querySelector("#population");
+const capitalElement = document.querySelector("#capital");
+const allCountriesButton = document.querySelector("#all-countries");
 const randomButton = document.querySelector("#random");
-const catgoriesButton = document.querySelector("#catagories");
+const quizButton = document.querySelector("#quiz");
 
-randomButton.addEventListener("click", function () {
-  const fetchRandomQuote = async () => {
+//Fetching all countries
+allCountriesButton.addEventListener("click", function () {
+  const fetchAllCountries = async () => {
     const response = await fetch(baseUrl);
-    const quotes = await response.json();
+    const countries = await response.json();
 
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    const randomQuote = quotes[randomIndex].text;
+    console.log(countries);
 
-    quoteTextElement.textContent = randomQuote;
-  };
+    //Sorting by common name
+    countries.sort((a, b) => a.name.common.localeCompare(b.name.common));
 
-  fetchRandomQuote();
-});
+    countriesWrapper.innerHTML = "";
 
-allQuotesButton.addEventListener("click", function () {
-  const fetchAllQuotes = async () => {
-    const response = await fetch(baseUrl);
-    const quotes = await response.json();
+    countries.forEach((country) => {
+      const countryDiv = document.createElement("div");
+      countryDiv.classList.add("country-div");
 
-    quoteWrapper.innerHTML = "";
+      const countryButton = document.createElement("div");
+      countryButton.classList.add("country-button");
 
-    quotes.forEach((quote) => {
-      const quoteElement = document.createElement("p");
-      quoteElement.textContent = quote.text;
-      quoteWrapper.appendChild(quoteElement);
+      const countryInfo = document.createElement("div");
+      countryInfo.classList.add("country-info");
+
+      const countryInfoText = document.createElement("div");
+      countryInfoText.classList.add("country-info-text");
+
+      const countryName = document.createElement("h2");
+      const moreButton = document.createElement("button");
+      moreButton.classList.add("show-more");
+
+      const countryContinent = document.createElement("p");
+      const countryPopulation = document.createElement("p");
+      const countryCapital = document.createElement("p");
+
+      const countryFlag = new Image(150);
+
+      countryName.textContent = country.name.common;
+      countryContinent.textContent = `Continent: ${country.continents}`;
+      countryPopulation.textContent = `Population: ${country.population}`;
+      countryCapital.textContent = `Capital: ${country.capital}`;
+      countryFlag.src = country.flags.png;
+
+      countryInfoText.appendChild(countryContinent);
+      countryInfoText.appendChild(countryPopulation);
+      countryInfoText.appendChild(countryCapital);
+
+      countryInfo.appendChild(countryInfoText);
+      countryInfo.appendChild(countryFlag);
+
+      countryButton.appendChild(countryName);
+      countryButton.appendChild(moreButton);
+
+      countryDiv.appendChild(countryButton);
+      countryDiv.appendChild(countryInfo);
+
+      countriesWrapper.appendChild(countryDiv);
+
+      //Expand
+      countryInfo.style.display = "none";
+      countryButton.addEventListener("click", function (e) {
+        moreButton.classList.toggle("close");
+        if (countryInfo.style.display === "none") {
+          countryInfo.style.display = "flex";
+        } else {
+          countryInfo.style.display = "none";
+        }
+      });
     });
   };
-  fetchAllQuotes();
+
+  fetchAllCountries();
+});
+
+//Fetching random countries
+randomButton.addEventListener("click", function () {
+  const fetchRandomCountry = async () => {
+    const response = await fetch(baseUrl);
+    const countries = await response.json();
+
+    const randomIndex = Math.floor(Math.random() * countries.length);
+    const randomCountry = countries[randomIndex].name.common;
+    const randomFlag = countries[randomIndex].flag;
+    const randomContinent = countries[randomIndex].continents;
+    const randomPopulation = countries[randomIndex].population;
+    const randomCapital = countries[randomIndex].capital;
+
+    countryTextElement.textContent = randomCountry;
+    flagElement.textContent = randomFlag;
+    continentElement.textContent = `Continent: ${randomContinent}`;
+    populationElement.textContent = `Population ${randomPopulation}`;
+    capitalElement.textContent = `Capital: ${randomCapital}`;
+  };
+
+  fetchRandomCountry();
 });
