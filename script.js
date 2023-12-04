a07e545a475d473448d555cad717bad733eea327
Julia <jsgoransson@gmail.com>
2023-12-04 11:07:46 +0100

    Fixes

--- script.js
+++ script.js
@@ -1,49 +1,42 @@
 const baseUrl = "https://restcountries.com/v3.1/all";
 const countriesWrapper = document.querySelector("#countries-wrapper");
-const countryTextElement = document.querySelector("#country-text");
-const flagElement = document.querySelector("#flag");
-const continentElement = document.querySelector("#continent");
-const populationElement = document.querySelector("#population");
-const capitalElement = document.querySelector("#capital");
 const allCountriesButton = document.querySelector("#all-countries");
 const randomButton = document.querySelector("#random");
-const quizButton = document.querySelector("#quiz");
+const gamesButton = document.querySelector("#games");
 
-//Fetching all countries
-allCountriesButton.addEventListener("click", function () {
-  const fetchAllCountries = async () => {
-    const response = await fetch(baseUrl);
-    const countries = await response.json();
+//Fetching countries
+const fetchCountries = async () => {
+  const response = await fetch(baseUrl);
+  const countries = await response.json();
 
-    console.log(countries);
+  //Showing all countries
 
-    //Sorting by common name
+  allCountriesButton.addEventListener("click", function () {
+    //Sorting alphabetically by common name
     countries.sort((a, b) => a.name.common.localeCompare(b.name.common));
 
     countriesWrapper.innerHTML = "";
 
+    // Creating div-element for every country
+
     countries.forEach((country) => {
       const countryDiv = document.createElement("div");
       countryDiv.classList.add("country-div");
-
       const countryButton = document.createElement("div");
       countryButton.classList.add("country-button");
-
       const countryInfo = document.createElement("div");
       countryInfo.classList.add("country-info");
-
       const countryInfoText = document.createElement("div");
       countryInfoText.classList.add("country-info-text");
-
       const countryName = document.createElement("h2");
       const moreButton = document.createElement("button");
       moreButton.classList.add("show-more");
-
       const countryContinent = document.createElement("p");
       const countryPopulation = document.createElement("p");
       const countryCapital = document.createElement("p");
-
       const countryFlag = new Image(150);
+
+      //Putting information from countries into divs
 
       countryName.textContent = country.name.common;
       countryContinent.textContent = `Continent: ${country.continents}`;
@@ -66,7 +59,7 @@ allCountriesButton.addEventListener("click", function () {
 
       countriesWrapper.appendChild(countryDiv);
 
-      //Expand
+      //Show more info when clicking the down-arrow
       countryInfo.style.display = "none";
       countryButton.addEventListener("click", function (e) {
         moreButton.classList.toggle("close");
@@ -77,30 +70,63 @@ allCountriesButton.addEventListener("click", function () {
         }
       });
     });
-  };
+  });
 
-  fetchAllCountries();
-});
+  //Showing random country
+  randomButton.addEventListener("click", function () {
+    countriesWrapper.innerHTML = "";
 
-//Fetching random countries
-randomButton.addEventListener("click", function () {
-  const fetchRandomCountry = async () => {
-    const response = await fetch(baseUrl);
-    const countries = await response.json();
+    // Getting random index
 
     const randomIndex = Math.floor(Math.random() * countries.length);
-    const randomCountry = countries[randomIndex].name.common;
-    const randomFlag = countries[randomIndex].flag;
-    const randomContinent = countries[randomIndex].continents;
-    const randomPopulation = countries[randomIndex].population;
-    const randomCapital = countries[randomIndex].capital;
 
-    countryTextElement.textContent = randomCountry;
-    flagElement.textContent = randomFlag;
-    continentElement.textContent = `Continent: ${randomContinent}`;
-    populationElement.textContent = `Population ${randomPopulation}`;
-    capitalElement.textContent = `Capital: ${randomCapital}`;
-  };
+    // Creating div-elements for random country
 
-  fetchRandomCountry();
+    const countryDiv = document.createElement("div");
+    countryDiv.classList.add("country-div");
+    const countryButton = document.createElement("div");
+    countryButton.classList.add("country-button");
+    const countryInfo = document.createElement("div");
+    countryInfo.classList.add("country-info");
+    const countryInfoText = document.createElement("div");
+    countryInfoText.classList.add("country-info-text");
+    const countryName = document.createElement("h2");
+    const countryContinent = document.createElement("p");
+    const countryPopulation = document.createElement("p");
+    const countryCapital = document.createElement("p");
+    const countryFlag = new Image(150);
+
+    //Putting information from random country into divs
+
+    countryName.textContent = countries[randomIndex].name.common;
+    countryContinent.textContent = `Continent: ${countries[randomIndex].continents}`;
+    countryPopulation.textContent = `Population: ${countries[randomIndex].population}`;
+    countryCapital.textContent = `Capital: ${countries[randomIndex].capital}`;
+    countryFlag.src = countries[randomIndex].flags.png;
+
+    countryInfoText.appendChild(countryContinent);
+    countryInfoText.appendChild(countryPopulation);
+    countryInfoText.appendChild(countryCapital);
+
+    countryInfo.appendChild(countryInfoText);
+    countryInfo.appendChild(countryFlag);
+
+    countryButton.appendChild(countryName);
+
+    countryDiv.appendChild(countryButton);
+    countryDiv.appendChild(countryInfo);
+
+    countriesWrapper.appendChild(countryDiv);
+  });
+};
+fetchCountries();
+
+//Games button message
+
+gamesButton.addEventListener("click", function () {
+  countriesWrapper.innerHTML = "";
+  const gamesDiv = document.createElement("div");
+  gamesDiv.classList.add("games-div");
+  gamesDiv.textContent = "Oh no! No games exist yet :(";
+  countriesWrapper.appendChild(gamesDiv);
 });
